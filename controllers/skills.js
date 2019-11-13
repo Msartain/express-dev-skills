775a21d496122566961f66d4de9e2fca0020aabd
Matthew Sartain <matthewsartain@me.com>
2019-11-13 16:18:49 -0600

    update skills page

---
+++ controllers/skills.js
@@ -0,0 +1,20 @@
+var Skill = require('../models/skills');
+
+module.exports = {
+  index,
+  show
+};
+
+function show(req, res) {
+  console.log(req.params.id);
+  res.render('skills/show', {
+    skills: Skill.getOne(req.params.id),
+    skillNum: parseInt(req.params.id) + 1
+  });
+}
+
+function index(req, res) {
+  res.render('skills/index', {
+    skills: Skill.getAll()
+  });
+}
